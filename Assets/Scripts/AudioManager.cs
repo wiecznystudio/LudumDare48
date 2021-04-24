69b6a113641af8dead7995d510f940c2ac070a0e
Wieczny <robertgalonpl@gmail.com>
2021-04-24 12:28:04 +0200

    Initial commit

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public static AudioManager instance;
+
+    #region Singleton
+    private void Awake() {
+        Singleton();
+    }
+    private void Singleton() {  
+        
+        if(instance != null) {
+            Debug.LogWarning("Detected more than one AudioManager instance!");
+        }
+        instance = this;
+    }
+    #endregion
+
+
+}
+
